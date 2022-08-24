c596d2c7718888d298921bce2c0dfa48d6606b3d
LucAle02 <lucia.solano@unmsm.edu.com>
2022-08-23 22:47:29 -0500

    A

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include "Game.hpp"
-#include "MainMenu.hpp"
 
 int main()
 {
-    sf::RenderWindow MENU(sf::VideoMode(1280,720),"MenuPrincipal",sf::Style::Default);
-    MainMenu mainMenu(MENU.getSize().x,MENU.getSize().y);
-    // Init Game
-    /*
     Game game;
     //Variables de tiempo para la animación
     sf::Clock clock;
@@ -24,6 +19,6 @@ int main()
         //Restart clock
         clock.restart();
     }
-    */
+    
     return 0;
 }
